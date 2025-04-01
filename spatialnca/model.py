3b002d8d5a44b1bd3d206119ac4a400fdaf4e485
dschaub95 <schaub.darius@gmail.com>
2025-04-01 10:59:20 +0200

    allow fixed node embeddings and fixed edge index

--- spatialnca/model.py
+++ spatialnca/model.py
@@ -5,23 +5,49 @@ from torch_geometric.nn import MLP, MessagePassing
 import torch_geometric as pyg
 from torch_scatter import scatter
 
+from spatialnca.layers.mlp import SimpleMLP
+from spatialnca.layers.egnn import EGNNLayer
+
 
 class SpatialNCA(nn.Module):
-    def __init__(self, input_dim, emb_dim, knn=10, reinit=False, **kwargs):
+    def __init__(
+        self,
+        input_dim,
+        emb_dim,
+        knn=10,
+        radius=None,
+        reinit=False,
+        skip_connections=True,
+        bounds=None,
+        use_fixed_emb=False,  # for debugging
+        fixed_edge_index=False,
+        **kwargs,
+    ):
         super().__init__()
         self.input_dim = input_dim
         self.emb_dim = emb_dim
         self.knn = knn
+        self.radius = radius
         self.reinit = reinit
-
-        self.mpnn = MPNNLayer(emb_dim, **kwargs)
+        self.skip_connections = skip_connections
+        self.bounds = bounds
+        self.mpnn = EGNNLayer(emb_dim, **kwargs)
         self.mlp_emb = SimpleMLP(
             in_channels=input_dim,
             out_channels=emb_dim,
-            hidden_channels=emb_dim,
-            n_layers=1,
+            n_layers=2,
             plain_last=True,
+            **kwargs,
         )
+        self.use_fixed_emb = use_fixed_emb
+        self.fixed_emb = None
+        self.fixed_edge_index = fixed_edge_index
+
+    def init_fixed_emb(self, n_cells):
+        if self.fixed_emb is None:
+            self.fixed_emb = nn.Parameter(
+                torch.randn(n_cells, self.emb_dim, device=self.device)
+            )
 
     def forward(self, h, pos, edge_index, h_init, edge_attr=None):
         # add initial node features
@@ -30,7 +56,10 @@ class SpatialNCA(nn.Module):
 
         # add residual connection
         h_update, pos_update = self.mpnn(h, pos, edge_index)
-        h = h + h_update
+        if self.skip_connections:
+            h = h + h_update
+        else:
+            h = h_update
         pos = pos + pos_update
         return h, pos
 
@@ -41,20 +70,18 @@ class SpatialNCA(nn.Module):
         edge_index = self.init_edge_index(pos) if edge_index is None else edge_index
 
         # embed the input features and store them
-        self.h_init = self.mlp_emb(x)
+        if self.use_fixed_emb:
+            self.init_fixed_emb(x.shape[0])
+            self.h_init = self.fixed_emb
+        else:
+            self.h_init = self.mlp_emb(x)
 
-        if h is None:
-            h = self.h_init
+        # start from the initial embedding if no (updated) h is provided
+        h = self.h_init if h is None else h
 
         # run for multiple steps
         for _ in range(n_steps):
-            h, pos = self.forward(h, pos, edge_index, h_init=self.h_init)
-
-            # confine pos to [0, 1] range
-            pos = torch.clamp(pos, min=0, max=1)
-
-            # update the edge index based on the new positions
-            edge_index = self.update_edge_index(pos, edge_index)
+            h, pos, edge_index = self.step(h, pos, edge_index)
 
             # compute intermediate loss (optional)
             if loss_fn is not None:
@@ -66,12 +93,61 @@ class SpatialNCA(nn.Module):
         else:
             return h, pos, edge_index
 
+    def step(self, h, pos, edge_index):
+        h, pos_new = self.forward(h, pos, edge_index, h_init=self.h_init)
+
+        # TODO track the change in pos per node (max, mean, median might be interesting)
+        # diff = torch.norm(pos_new - pos, p=2, dim=-1)
+        # mean_diff = diff.mean().item()
+        # max_diff = diff.max().item()
+        # min_diff = diff.min().item()
+        # median_diff = torch.median(diff).item()
+        
+        pos = pos_new
+
+        # confine pos to bound range
+        if self.bounds is not None:
+            pos = torch.clamp(pos, min=self.bounds[0], max=self.bounds[1])
+
+        # update the edge index based on the new positions
+        if not self.fixed_edge_index:
+            edge_index = self.update_edge_index(pos, edge_index)
+
+        return h, pos, edge_index
+
     def update_edge_index(self, pos, edge_index=None, batch=None):
-        # this graph is directed, exclude self loops for now, automatically handles device
-        edge_index = pyg.nn.knn_graph(
-            pos, k=self.knn, loop=False, flow="source_to_target", batch=batch
-        )
-        return edge_index
+        # Ensure at least one of radius or k-NN is specified
+        assert self.radius or self.knn, "Either radius or knn must be specified"
+
+        combined_edge_index = []
+
+        # Compute radius graph if radius is specified
+        if self.radius:
+            radius_edge_index = pyg.nn.radius_graph(
+                pos,
+                r=self.radius,
+                loop=False,
+                batch=batch,
+                flow="source_to_target",
+                max_num_neighbors=64,
+            )
+            combined_edge_index.append(radius_edge_index)
+
+        # Compute k-NN graph if k is specified
+        if self.knn:
+            knn_edge_index = pyg.nn.knn_graph(
+                pos, k=self.knn, loop=False, flow="source_to_target", batch=batch
+            )
+            combined_edge_index.append(knn_edge_index)
+
+        if len(combined_edge_index) > 1:
+            combined_edge_index = torch.cat(combined_edge_index, dim=1)
+            # Remove duplicate edges
+            combined_edge_index = torch.unique(combined_edge_index, dim=1)
+        else:
+            # Use the single available edge index
+            combined_edge_index = combined_edge_index[0]
+        return combined_edge_index
 
     def init_edge_index(self, pos, **kwargs):
         return self.update_edge_index(pos, **kwargs)
@@ -81,98 +157,15 @@ class SpatialNCA(nn.Module):
         return next(self.parameters()).device
 
 
-class MPNNLayer(MessagePassing):
-    def __init__(
-        self,
-        emb_dim,
-        hidden_dim=128,
-        n_layers_msg=2,
-        n_layers_upd=1,
-        n_layers_pos=2,
-        aggr="sum",
-        aggr_pos="mean",
-        **kwargs,
-    ):
-        super().__init__(aggr=aggr)
-        self.aggr_pos = aggr_pos
-
-        self.mlp_msg = SimpleMLP(
-            in_channels=emb_dim * 2 + 1,
-            out_channels=emb_dim,
-            hidden_channels=hidden_dim,
-            n_layers=n_layers_msg,
-            **kwargs,
+def construct_graph(pos, radius=None, knn=None, batch=None):
+    if radius is not None:
+        edge_index = pyg.nn.radius_graph(
+            pos, r=radius, loop=True, flow="source_to_target", batch=batch
         )
-        self.mlp_upd = SimpleMLP(
-            in_channels=emb_dim * 2,
-            out_channels=emb_dim,
-            hidden_channels=hidden_dim,
-            n_layers=n_layers_upd,
-            **kwargs,
+    elif knn is not None:
+        edge_index = pyg.nn.knn_graph(
+            pos, k=knn, loop=True, flow="source_to_target", batch=batch
         )
-        self.mlp_pos = SimpleMLP(
-            in_channels=emb_dim,
-            out_channels=1,
-            hidden_channels=hidden_dim,
-            n_layers=n_layers_pos,
-            **kwargs,
-        )
-
-    def forward(self, h, pos, edge_index, edge_attr=None):
-        # compute initial feature transfo
-        out = self.propagate(edge_index, h=h, x=pos, edge_attr=edge_attr)
-        return out
-
-    def message(self, h_i, h_j, x_i, x_j, edge_attr):
-        diff = x_j - x_i
-        dist = torch.norm(diff, p=2, dim=-1, keepdim=True)
-
-        msg = torch.cat([h_i, h_j, dist], dim=-1)
-        msg = self.mlp_msg(msg)
-
-        # intuitively determines in which direction to move
-        # might be imporvable by using the message and the aggregated message
-        # to get a full picture before determinign waht do focus on
-        diff_scaled = diff * self.mlp_pos(msg)
-
-        return msg, diff_scaled
-
-    def aggregate(self, inputs, index):
-        node_msg, pos_msg = inputs
-        h_aggr = scatter(node_msg, index, dim=self.node_dim, reduce=self.aggr)
-        x_aggr = scatter(pos_msg, index, dim=self.node_dim, reduce=self.aggr_pos)
-        return h_aggr, x_aggr
-
-    def update(self, aggr_out, h):
-        h_aggr, pos_upd = aggr_out
-        h_upd = torch.cat([h, h_aggr], dim=-1)
-        h_upd = self.mlp_upd(h_upd)
-        return h_upd, pos_upd
-
-
-class SimpleMLP(MLP):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        hidden_channels=None,
-        n_layers=1,
-        act="relu",
-        dropout=0.0,
-        norm="batch_norm",
-        plain_last=False,
-        **kwargs,
-    ):
-        hidden_channels = out_channels if hidden_channels is None else hidden_channels
-        super().__init__(
-            channel_list=None,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            hidden_channels=hidden_channels,
-            num_layers=n_layers,
-            act=act,
-            dropout=dropout,
-            norm=norm,
-            plain_last=plain_last,
-            **kwargs,
-        )
+    else:
+        raise ValueError("Either radius or knn must be specified")
+    return edge_index
