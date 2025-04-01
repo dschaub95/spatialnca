d11463bb5cf06734fe1a3b26fc027c13afc024ef
dschaub95 <schaub.darius@gmail.com>
2025-04-01 10:57:10 +0200

    refactor trainer

--- spatialnca/train.py
+++ spatialnca/train.py
@@ -2,9 +2,11 @@ import torch
 import torch.nn.functional as F
 from tqdm.auto import tqdm
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from spatialnca.model import SpatialNCA
+from spatialnca.spatial import uniform_point_cloud
 
 
 class Trainer:
@@ -49,40 +51,81 @@ class Trainer:
         self.dists_true = dists_true
 
         # setup optimizer
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.setup_optimizer()
 
         # setup history
-        self.history = {"train_loss": []}
+        self.history = []
+
+    def setup_optimizer(self):
+        # setup optimizer
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
 
     def train(self, data):
         self.setup_training(data)
 
-        for epoch in tqdm(range(self.n_epochs)):
+        prog_bar = tqdm(range(self.n_epochs), desc="Training")
+
+        for epoch in prog_bar:
             # reinit node positions regularly
             if epoch % self.reintv == 0:
-                data.pos_init = torch.randn(data.pos.shape[0], 2, device=self.device)
-            self.train_one_epoch(data)
+                self.init_node_positions(data)
+
+            # train one epoch
+            loss = self.train_one_epoch(data)
+
+            # store history
+            self.history.append({"epoch": epoch, "train_loss": loss.item()})
+
+            # dynamically update progress bar
+            prog_bar.set_postfix(
+                {k: v for k, v in self.history[-1].items() if k != "epoch"}
+            )
 
     def train_one_epoch(self, data):
+        # sample number of steps
         n_steps = self.step_sampler.sample()
+
+        # rollout
         h, pos, edge_index = self.model.rollout(
             x=data.x, pos=data.pos_init, n_steps=n_steps, edge_index=data.edge_index
         )
+
+        # compute loss
         dists_pred = torch.norm(
             pos[self.edge_index_full[0]] - pos[self.edge_index_full[1]], p=2, dim=-1
         )
         loss = F.mse_loss(dists_pred, self.dists_true)
+
+        # backprop
         self.optimizer.zero_grad()
         loss.backward()
 
         torch.nn.utils.clip_grad_value_(self.model.parameters(), self.clip_value)
         self.optimizer.step()
 
-        self.history["train_loss"].append(loss.item())
+        return loss
 
     def plot_history(self):
-        plt.plot(self.history["train_loss"])
+        df = pd.DataFrame(self.history)
+        plt.plot(df["train_loss"])
         plt.show()
+
+    def init_node_positions(self, data):
+        data.pos_init = 1 * torch.randn(data.pos.shape[0], 2, device=self.device)
+        # data.pos_init = torch.zeros(data.pos.shape[0], 2, device=self.device)
+
+        # data.pos_init = torch.tensor(
+        #     uniform_point_cloud(data.num_nodes, 0.15),
+        #     device=self.device,
+        # )
+
+        # median_dist = self.dists_true.median()
+        # data.pos_init = torch.tensor(
+        #     uniform_point_cloud(data.num_nodes, median_dist / 2),
+        #     device=self.device,
+        # )
+        # adapt the model radius to the scale of the data
+        # self.model.radius = 2 * median_dist
 
 
 class Tester:
