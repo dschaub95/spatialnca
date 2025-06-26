d0b580cea58698162f78601fb13fc394c86c1563
dschaub95 <schaub.darius@gmail.com>
2025-06-26 16:32:34 +0200

    ensure venv is active

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,4 +1,5 @@
 uv venv --python 3.12
+source .venv/bin/activate
 uv sync
 uv pip install torch_scatter torch_cluster pyg_lib torch_sparse -f https://data.pyg.org/whl/torch-2.7.0+cu126.html
 uv pip install -e .
