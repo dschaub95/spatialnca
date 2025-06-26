b6ad5e251d095f55d1f0c9514d52d8a12b84dc04
dschaub95 <schaub.darius@gmail.com>
2025-06-26 17:50:19 +0200

    fix venv setup

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,7 +1,5 @@
-unset VIRTUAL_ENV
-export UV_LINK_MODE=symlink
-
-uv venv --python 3.12
+rm -r .venv
+uv venv
 source .venv/bin/activate
 uv sync
 uv pip install torch_scatter torch_cluster pyg_lib torch_sparse -f https://data.pyg.org/whl/torch-2.7.0+cu126.html
