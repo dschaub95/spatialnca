f573ec235357a94e7c47a89c8e286f5c2f1e223b
dschaub95 <schaub.darius@gmail.com>
2025-07-01 11:09:52 +0200

    add cli support via fire

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+import fire
+import sys
 from spatialnca.config import Config
 from spatialnca.train import train
 
+
+def main(**kwargs):
+    cfg = Config(**kwargs)
+    train(cfg, print_cfg=True)
+
+
 if __name__ == "__main__":
-    train(Config(), print_cfg=True)
+    # Call Fire only if there actually are CLI arguments
+    if len(sys.argv) > 1:
+        fire.Fire(main)
+    else:
+        main()
